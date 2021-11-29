58cf34476c73d94669b932da032a733711d7372a
Ahmad El Mokahal <a.mokahal_99@hotmail.com>
2021-11-29 16:51:02 -0500

    Added comments to UART file

--- Project/UART/UART.vhd
+++ Project/UART/UART.vhd
@@ -1,1 +1,27 @@
 --- This file will include: Receiver, Transmitter, BaudRateGenerator ---
+entity UART is
+  port (
+    i_clock : IN STD_LOGIC;
+    i_reset : IN STD_LOGIC;
+    i_Data : IN STD_LOGIC_VECTOR(7 downto 0);  --Input from the BUS
+    i_SEL : IN STD_LOGIC_VECTOR(2 downto 0); 
+    i_RxD : IN STD_LOGIC;
+    o_Data : OUT STD_LOGIC_VECTOR(7 downto 0); --Output to the BUS
+    o_TxD : OUT STD_LOGIC
+  ) ;
+end UART ;
+
+architecture arch of UART is
+    --- Transmitter ---
+
+    --- Receiver ---
+
+    --- Baud Rate Generator ---
+
+    --- enASdFF --- (For TDRE & RDRF)
+
+begin
+
+
+
+end architecture ; -- arch
